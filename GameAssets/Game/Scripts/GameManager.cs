87c4145221fe4487050c325bb8ab9ab6110a2e6d
izz13 <94653804+izz13@users.noreply.github.com>
2025-11-15 18:35:31 -0800

    Rename UI labels for unit stats in Game UI
    Changed label names from 'Health', 'Hunger', and 'Level' to 'Label0', 'Label1', and 'Label2' in GameUI.uxml and updated GameManager.cs to reference the new label names. This improves consistency and flexibility in the UI code.

--- GameAssets/Game/Scripts/GameManager.cs
+++ GameAssets/Game/Scripts/GameManager.cs
@@ -75,9 +75,9 @@ public class GameManager : MonoBehaviour
             //3. to get the stats use moundMananger, for example if I wanted the queen health
             //example : moundManager.queenAnt.health;
             gameUI.rootVisualElement.Q<Label>("Unit_Title").text = moundManager.queenAnt.name;
-            gameUI.rootVisualElement.Q<Label>("Health").text = "Health: " + moundManager.queenAnt.health.ToString();
-            gameUI.rootVisualElement.Q<Label>("Hunger").text = "Hunger: " + moundManager.queenAnt.hunger.ToString();
-            gameUI.rootVisualElement.Q<Label>("Level").text = "Level: " + moundManager.queenAnt.level.ToString();
+            gameUI.rootVisualElement.Q<Label>("Label0").text = "Health: " + moundManager.queenAnt.health.ToString();
+            gameUI.rootVisualElement.Q<Label>("Label1").text = "Hunger: " + moundManager.queenAnt.hunger.ToString();
+            gameUI.rootVisualElement.Q<Label>("Label2").text = "Level: " + moundManager.queenAnt.level.ToString();
 
         }
         else if (antManager.ants.Count > 0)
@@ -86,16 +86,16 @@ public class GameManager : MonoBehaviour
             //1.Do the same thing you did for queen
             //2.But for firstAnt instead
             gameUI.rootVisualElement.Q<Label>("Unit_Title").text = firstAnt.name;
-            gameUI.rootVisualElement.Q<Label>("Hunger").text = "";
-            gameUI.rootVisualElement.Q<Label>("Health").text = "Health: "+firstAnt.health.ToString();
-            gameUI.rootVisualElement.Q<Label>("Level").text = "Level: " +firstAnt.level.ToString();
+            gameUI.rootVisualElement.Q<Label>("Label0").text = "";
+            gameUI.rootVisualElement.Q<Label>("Label1").text = "Health: "+firstAnt.health.ToString();
+            gameUI.rootVisualElement.Q<Label>("Label2").text = "Level: " +firstAnt.level.ToString();
         }
         else
         {
             gameUI.rootVisualElement.Q<Label>("Unit_Title").text = "";
-            gameUI.rootVisualElement.Q<Label>("Health").text = "";
-            gameUI.rootVisualElement.Q<Label>("Hunger").text = "";
-            gameUI.rootVisualElement.Q<Label>("Level").text = "";
+            gameUI.rootVisualElement.Q<Label>("Label0").text = "";
+            gameUI.rootVisualElement.Q<Label>("Label1").text = "";
+            gameUI.rootVisualElement.Q<Label>("Label2").text = "";
         }
         // if (!selectedGameObjects.Contains(moundManager.gameObject) && selectedGameObjects.Contains(antManager.gameObject))
         // {
